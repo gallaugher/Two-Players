595cca483c880e541c7e919e106d536d3338dedd
Gallaugher <john.gallaugher@bc.edu>
2017-09-19 19:01:29 -0400

    Solution to 3.2.1 Two Players in http://gallaugher.com/swift

--- Two Players/ViewController.swift
+++ Two Players/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerOneLabel: UILabel!
+    @IBOutlet weak var playerTwoLabel: UILabel!
+    @IBOutlet weak var beginGameButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    var playersEntered = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        beginGameButton.isHidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        
+        if playersEntered == 0 {
+            playerOneLabel.text = "Player 1: " + nameTextField.text!
+            nameTextField.text = ""
+            playersEntered += 1
+            
+        } else {
+            playerTwoLabel.text = "Player 2: " + nameTextField.text!
+            nameTextField.text = ""
+            playersEntered += 1
+            beginGameButton.isHidden = false
+            nameTextField.isEnabled = false
+        }
     }
-
-
 }
 
